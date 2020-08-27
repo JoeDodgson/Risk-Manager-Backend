3e15216cabb95d53b06772f51a90f588b5afaf65
ian <chengkwanyu@yahoo.com.hk>
2020-08-27 13:10:39 +0800

    file name changed and add controller to change data in company schema

--- controllers/routes.js
+++ controllers/routes.js
@@ -1,10 +1,11 @@
 // File collect all routes and export in one folder
-const auth = require ("./models/authorisation")
-const disciplines = require ("./models/disciplines")
-const project = require ("./models/project")
-const risk = require ("./models/risk")
-const status = require ("./models/status")
-const user = require ("./models/user")
+const auth = require ("./models/authorisation");
+const disciplines = require ("./models/disciplines");
+const project = require ("./models/project");
+const risk = require ("./models/risk");
+const status = require ("./models/status");
+const user = require ("./models/user");
+const company = require("./models/company");
 
 
 module.exports = {
@@ -15,11 +16,13 @@ module.exports = {
     createRisk: risk[0],
     createStatus: status[0],
     createUser: user[0],
+    createCompany: company[0],
     // Delete
     deleteAuth: auth[1],
     deleteDiscipline: disciplines[1],
     deleteProject: project[1],
     deleteStatus: status[1],
+    deleteCompany: company[1],
     // Get
     getRisk: risk[2],
     getUserData: user[2],
