5a6c7d3bd96dbff5971751e6c02442ee584675d5
niroshanwitharana <nrosh_uoc@yahoo.com>
2020-09-03 17:13:44 +0100

    project model updated with new info and validations

--- db/models/project.js
+++ db/models/project.js
@@ -6,6 +6,30 @@ const ProjectSchema = new Schema({
     title: {
         type: String,
         validate: [({ length }) => length <= 100, "Title should be within 100 characters."]
+    },
+    description: {
+        type: String,
+        validate: [({ length }) => length <= 1000, "Title should be within 1000 characters."]
+    },
+    location: {
+        type: Array,
+        required: true 
+    },
+    startDate: {
+        type: Date,
+        required: true 
+    },
+    endDate: {
+        type: Date,
+        required: true 
+    },
+    client: {
+        type: String,
+        required: true 
+    },
+    teamMembers: {
+        type: Array,
+        required: true 
     }
 });
 
