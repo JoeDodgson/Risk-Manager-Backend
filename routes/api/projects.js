8b72e377c3b9c7e51fad83bb8063381afee48d48
niroshanwitharana <nrosh_uoc@yahoo.com>
2020-09-03 17:26:42 +0100

    created files to add a middleware(authentication or authorisation) and follow the standerd file structure

---
+++ routes/api/projects.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const passport = require('passport');
+const app = express();
+const projectRouter = require("express").Router();
+const riskController = require("../../controllers/index");
+
+const passportJWT = passport.authenticate('jwt', {session : false});
+
+// creating a new project and delete existing project
+projectRouter.get("/", passportJWT,  riskController.getAllProjects);
+projectRouter.post("/", passportJWT,  riskController.createProject);
+projectRouter.delete("/:id", passportJWT, riskController.deleteProject);
+
+// craeting risk, get them and update them
+projectRouter.post("/risk", passportJWT, riskController.createRisk);
+projectRouter.route("/risk/:id", passportJWT)
+    .get(riskController.getRisk)
+    .put(riskController.changeRisk);
+
+module.exports = projectRouter;
