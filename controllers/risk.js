7fe2b265b7bf89668397bb8b1c2c2e25a013cfea
Joe Dodgson <j.dodgson@live.co.uk>
2020-09-05 18:12:13 +0100

    Refactored risk.js to move res.status() and .json() onto separate lines for consistency

--- controllers/risk.js
+++ controllers/risk.js
@@ -11,12 +11,14 @@ module.exports = {
     Risk.findOne({ title }).then((data) => {
       // If the risk exists in the DB, return 'bad request' 400 code
       if (data) {
-        res.status(400).json({
-          message: {
-            msgBody: "This Risk is already start taking control",
-            msgErr: true,
-          },
-        });
+        res
+          .status(400)
+          .json({
+            message: {
+              msgBody: "This Risk is already start taking control",
+              msgErr: true,
+            },
+          });
       }
       // Else save the new risk to the collection using Mongoose
       else {
@@ -49,22 +51,26 @@ module.exports = {
         newRisk.save((err, newRiskData) => {
           if (err) {
             // If error occurs when saving to DB, return 500 'Internal Server Error' code
-            res.status(500).json({
-              message: { 
-                msgBody: "An error occured creating your new risk",
-                msgErr: true,
-              },
-            });
+            res
+              .status(500)
+              .json({
+                message: { 
+                  msgBody: "An error occured creating your new risk",
+                  msgErr: true,
+                },
+              });
           }
           else {
             // If no error occurred, new risk was created so return 201 'Created' success code
-            res.status(201).json({
-              message: {
-                msgBody: "New risk successfully created",
-                msgErr: false,
-              },
-              data: { newRiskData },
-            });
+            res
+              .status(201)
+              .json({
+                message: {
+                  msgBody: "New risk successfully created",
+                  msgErr: false,
+                },
+                data: { newRiskData },
+              });
           }
         });
       }
@@ -77,13 +83,15 @@ module.exports = {
       .then((risk, err) => {
         // If risk exists in DB return to the user with 200 'OK' code
         if (risk)
-          res.status(200).json({
-            message: {
-              msgBody: "Risk successfully returned",
-              msgErr: false,
-            },
-            data: { risk },
-          });
+          res
+            .status(200)
+            .json({
+              message: {
+                msgBody: "Risk successfully returned",
+                msgErr: false,
+              },
+              data: { risk },
+            });
       })
       // If the risk ID could not be found in the DB, return a 404 'Not Found' code
       .catch((err) => {
@@ -137,13 +145,15 @@ module.exports = {
     )
     .then((updatedRisk) => {
       // If risk was successfully updated in DB, return to the user with 200 'OK' code
-      res.status(200).json({
-        message: {
-          msgBody: "Risk successfully updated",
-          msgErr: false,
-        },
-        data: { updatedRisk },
-      });
+      res
+        .status(200)
+        .json({
+          message: {
+            msgBody: "Risk successfully updated",
+            msgErr: false,
+          },
+          data: { updatedRisk },
+        });
     })
     .catch((err) =>
       // If the risk was not updated in the DB, return a 422 'Unprocessable Entity' code
