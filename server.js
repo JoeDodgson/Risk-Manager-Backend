be0ea731b9eec90f2abfbea6a0b01f4fbba4d892
Joe Dodgson <j.dodgson@live.co.uk>
2020-09-17 22:23:29 +0100

    Specified front end app URL in Access-Control-Allow-Origin

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use(cors());
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Origin', 'https://riskmanager-jmni.web.app/');
     next();
 });
 
