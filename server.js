ba45457eef7353327abd1f9d207f5d6549486afd
Joe Dodgson <j.dodgson@live.co.uk>
2020-09-18 21:47:31 +0100

    Added cors option, removed "Access-Control-Allow-Headers" headers

--- server.js
+++ server.js
@@ -22,15 +22,17 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
 
+app.options("*", cors());
+
 app.use((req, res, next) => {
   res.header(
     "Access-Control-Allow-Origin",
     "https://riskmanager-jmni.web.app/"
   );
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
   next();
 });
 
