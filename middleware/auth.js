ed5c95db2e9f1bd6f1aad077360bf3e0e51d718f
meeday <59993824+meeday@users.noreply.github.com>
2020-09-23 15:34:54 +0100

    auth middleware created

--- middleware/auth.js
+++ middleware/auth.js
@@ -4,16 +4,28 @@ const auth = (req, res, next) => {
   try {
     const token = req.header("x-auth-token");
     if (!token)
-      return res.status(401).json({ message: "Authorization Denied" });
+      return res.status(401).json({
+        isAuthenticated: false,
+        message: {
+          msgBody: "Authorization Failed",
+          msgErr: true,
+        },
+      });
 
     const verified = jwt.verify(token, process.env.JWT_SECRET);
     if (!verified)
-      return res
-        .status(401)
-        .json({ message: "Token Verification Failed, Authorization Denied" });
+      return res.status(401).json({
+        isAuthenticated: false,
+        message: {
+          msgBody: "Token Verification Failed, Access Denied",
+          msgErr: true,
+        },
+      });
     res.user = verified.id;
     next();
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
 };
+
+module.exports = auth;
