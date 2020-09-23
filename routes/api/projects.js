67e82386615f7abcfd8ed00e8a267d5679879430
meeday <59993824+meeday@users.noreply.github.com>
2020-09-23 15:35:10 +0100

    auth middleware imported into projects route

--- routes/api/projects.js
+++ routes/api/projects.js
@@ -1,29 +1,26 @@
 // Require in local files and npm modules
-const express = require('express');
-const passport = require('passport');
 const projectRouter = require("express").Router();
 const riskController = require("../../controllers/index");
-
+const auth = require("../../middleware/auth")
 // Create an express server
 const app = express();
 
 // Define passportJWT middleware
-const passportJWT = passport.authenticate('jwt', {session : false});
 
 // Define routes for project CRUD operations, using the riskController
-projectRouter.get("/", passportJWT,  riskController.getAllProjects);
-projectRouter.get("/:id", passportJWT,  riskController.getProject);
-projectRouter.post("/", passportJWT,  riskController.createProject);
-projectRouter.delete("/:id", passportJWT, riskController.deleteProject);
-projectRouter.get("/user/:id", passportJWT, riskController.getProjectByUserId);
+projectRouter.get("/", auth,  riskController.getAllProjects);
+projectRouter.get("/:id", auth,  riskController.getProject);
+projectRouter.post("/", auth,  riskController.createProject);
+projectRouter.delete("/:id", auth, riskController.deleteProject);
+projectRouter.get("/user/:id", auth, riskController.getProjectByUserId);
 
 // Define routes for risk CRUD operations, using the riskController
-projectRouter.post("/risk", passportJWT, riskController.createRisk);
-projectRouter.get("/risk/:id", passportJWT, riskController.getRisk);
-projectRouter.put("/risk/:id", passportJWT, riskController.changeRisk);
-projectRouter.delete("/risk/:id", passportJWT, riskController.deleteRisk);
-projectRouter.put("/risk/:id/comment", passportJWT, riskController.createComment);
-projectRouter.get("/risk/user/:id", passportJWT, riskController.getRisksByUserId);
-projectRouter.get("/risk/project/:id", passportJWT, riskController.getRisksByProjectId);
+projectRouter.post("/risk", auth, riskController.createRisk);
+projectRouter.get("/risk/:id", auth, riskController.getRisk);
+projectRouter.put("/risk/:id", auth, riskController.changeRisk);
+projectRouter.delete("/risk/:id", auth, riskController.deleteRisk);
+projectRouter.put("/risk/:id/comment", auth, riskController.createComment);
+projectRouter.get("/risk/user/:id", auth, riskController.getRisksByUserId);
+projectRouter.get("/risk/project/:id", auth, riskController.getRisksByProjectId);
 
 module.exports = projectRouter;
